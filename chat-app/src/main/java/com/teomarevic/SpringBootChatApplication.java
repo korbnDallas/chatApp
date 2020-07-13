53e2ff33194f84a16ba42f2628e7a1648fbca0fd
teo <teo@DESKTOP-I3IFSN7>
2020-07-13 11:23:42 +0200

    First commit

---
+++ chat-app/src/main/java/com/teomarevic/SpringBootChatApplication.java
@@ -0,0 +1,14 @@
+package com.teomarevic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootChatApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(
+				 SpringBootChatApplication.class , args);
+	}
+}
